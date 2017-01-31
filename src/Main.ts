3224ee5c2fcbef497f13dc25d5b2463e58501f35
Right2Drive <bradpf@outlook.com>
2017-01-30 19:40:06 -0500

    Project setup

---
+++ src/Main.ts
@@ -0,0 +1,40 @@
+/**
+ * Entry file for the electron application. 
+ * 
+ * Written from a template found here [http://blog.dmbcllc.com/typescript-and-electron-the-right-way/]
+ */
+
+import {BrowserWindow} from 'electron';
+
+export default class Main {
+    static mainWindow: Electron.BrowserWindow;
+    static application: Electron.App;
+    static BrowserWindow;
+    
+    private static onWindowAllClosed() {
+        if (process.platform !== 'darwin') {
+            Main.application.quit();
+        }
+    }
+
+    private static onClose() {
+        // Dereference the window object.
+        Main.mainWindow = null;
+    }
+
+    private static onReady() {
+        Main.mainWindow = new Main.BrowserWindow({width: 800, height: 600})
+        Main.mainWindow.loadURL('file://' + __dirname + '/../src/index.html');
+        Main.mainWindow.on('closed', Main.onClose);
+    }
+
+    static main(app: Electron.App, browserWindow: typeof BrowserWindow) {
+        // we pass the Electron.App object and the Electron.BrowserWindow into this function
+        // so this class has no dependencies. This makes the code easier to write tests for.
+        
+        Main.BrowserWindow = browserWindow;
+        Main.application = app;
+        Main.application.on('window-all-closed', Main.onWindowAllClosed);
+        Main.application.on('ready', Main.onReady);
+    }
+}
