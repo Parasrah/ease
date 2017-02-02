8109c63d3b93fef24c34f69180e567790088ad38
Brad Pfannmuller <bradpf@outlook.com>
2017-02-02 00:45:58 -0500

    Styling main page

--- src/ts/render/upload.ts
+++ src/ts/render/upload.ts
@@ -6,7 +6,7 @@ import { ipcRenderer } from 'electron';
 
 document.addEventListener('DOMContentLoaded', (event) => {
     
-    let uploadClick = document.querySelector('.test-click');
+    let uploadClick = document.querySelector('.upload-click');
     
     uploadClick.addEventListener('click', () => {
         ipcRenderer.send('upload-click');
