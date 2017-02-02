8dca548cc38680092ae8d391342a3fd49e4c0558
Brad Pfannmuller <bradpf@outlook.com>
2017-02-02 01:24:24 -0500

    Refactoring IndexListener

--- src/ts/listeners/IndexListener.ts
+++ src/ts/listeners/IndexListener.ts
@@ -1,18 +1,19 @@
 import { ipcMain, dialog } from 'electron';
 
 // Setup the event listeners for the index
-export let setupIndex = (browserWindow: Electron.BrowserWindow) => {
+export class IndexListener {
 
-    ipcMain.on('upload-click', () => {
-        dialog.showOpenDialog(browserWindow, {
-            title: 'Select your movie',
-            properties: ['openFile'],
-            filters: [
-                {name: 'Movies', extensions: ['mkv', 'avi', 'mp4']},
-            ]
-        }, (fileNames) => {
-            console.log(fileNames.length);
+    static setupIndex = (browserWindow: Electron.BrowserWindow) => {
+        ipcMain.on('upload-click', () => {
+            dialog.showOpenDialog(browserWindow, {
+                title: 'Select your movie',
+                properties: ['openFile'],
+                filters: [
+                    {name: 'Movies', extensions: ['mkv', 'avi', 'mp4']},
+                ]
+            }, (fileNames) => {
+                console.log(fileNames.length);
+            });
         });
-    });
-
-};
+    };
+} 
