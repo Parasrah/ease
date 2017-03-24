80a2f2769d1beb7b16af8ebfa7c6d652337602d0
Right2Drive <bradpf@outlook.com>
2017-03-23 22:10:18 -0400

    test HostPeerManager#receiveSignalData

--- test/unit/render/main_process/peer/HostPeerManagerTest.ts
+++ test/unit/render/main_process/peer/HostPeerManagerTest.ts
@@ -63,6 +63,42 @@ describe("HostPeerManager Unit Tests", function() {
             // Verify signal data was delivered to peer
             td.verify(mockSignal(mockSignalData));
         });
+
+        it("Should find peer among an array of peers and deliver signal data", function() {
+             // Create the mocked peer
+            const mockSignal = td.function();
+            const mockPeer = {
+                signal: mockSignal,
+                clientID,
+            };
+
+            // Add some peers with different clientID's
+            function createPeer(clientID: string) {
+                return Object.freeze({
+                    clientID,
+                });
+            }
+
+            const mockedPeers = [];
+
+            // Add some bad peers
+            mockedPeers.push(createPeer(clientID + "_changed"));
+            mockedPeers.push(createPeer("$p3c14L-ch4r4ch74r5-*"));
+            mockedPeers.push(createPeer(clientID.toUpperCase()));
+
+            // Insert the mocked peer at end of array
+            mockedPeers.push(mockPeer);
+
+            // Add mockedPeers to HostPeerManager peers
+            (subject as any).peers.push(...mockedPeers);
+
+            // Deliver signal data
+            (subject as any).recieveSignalData(clientID, mockSignalData);
+
+            // Verify signal data was delivered to peer
+            td.verify(mockSignal(mockSignalData));
+        });
+
     });
 
     describe("#createPeer", function() {
