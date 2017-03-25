e5250f3b91235e5869a8a4d0e1be030ffc7b4fc9
Right2Drive <bradpf@outlook.com>
2017-03-24 20:49:26 -0400

    Adding receiveSignalData and setupPeer tests

--- test/unit/render/main_process/peer/HostPeerManagerTest.ts
+++ test/unit/render/main_process/peer/HostPeerManagerTest.ts
@@ -1,14 +1,25 @@
 import { assert as Assert } from "chai";
 import * as td from "testdouble";
 
-import { HostPeerManager } from "../../../../../src/render/peer/HostPeerManager";
+import { HostPeerManager, IEnhancedPeer } from "../../../../../src/render/peer/HostPeerManager";
 
 describe("HostPeerManager Unit Tests", function() {
     let subject: HostPeerManager;
     let clientID: string;
+    let mockSignalData: any;
+    let badClientIdList: string[];
 
     before(function() {
         clientID = "test-client-id";
+        mockSignalData = Object.freeze({
+            test: "This is mock signal data!",
+        });
+
+        badClientIdList = [];
+        badClientIdList.push(clientID + "_changed");
+        badClientIdList.push("$p3c14L-ch4r4ch74r5-*");
+        badClientIdList.push(clientID.toUpperCase());
+        Object.freeze(badClientIdList);
     });
 
     beforeEach(function() {
@@ -40,13 +51,6 @@ describe("HostPeerManager Unit Tests", function() {
     });
 
     describe("#receiveSignalData", function() {
-        let mockSignalData: any;
-
-        before(function() {
-            mockSignalData = Object.freeze({
-                test: "This is mock signal data!",
-            });
-        });
 
         it("Should signal peer if peer is present", function() {
             // Create the mocked peer
@@ -58,7 +62,7 @@ describe("HostPeerManager Unit Tests", function() {
             (subject as any).peers.push(mockPeer);
 
             // Push mock signal data to host peer manager
-            (subject as any).recieveSignalData(clientID, mockSignalData);
+            (subject as any).receiveSignalData(clientID, mockSignalData);
 
             // Verify signal data was delivered to peer
             td.verify(mockSignal(mockSignalData));
@@ -72,19 +76,10 @@ describe("HostPeerManager Unit Tests", function() {
                 clientID,
             };
 
-            // Add some peers with different clientID's
-            function createPeer(clientID: string) {
-                return Object.freeze({
-                    clientID,
-                });
-            }
-
             const mockedPeers = [];
 
             // Add some bad peers
-            mockedPeers.push(createPeer(clientID + "_changed"));
-            mockedPeers.push(createPeer("$p3c14L-ch4r4ch74r5-*"));
-            mockedPeers.push(createPeer(clientID.toUpperCase()));
+            badClientIdList.map((value) => mockedPeers.push(createPeer(value)));
 
             // Insert the mocked peer at end of array
             mockedPeers.push(mockPeer);
@@ -93,24 +88,120 @@ describe("HostPeerManager Unit Tests", function() {
             (subject as any).peers.push(...mockedPeers);
 
             // Deliver signal data
-            (subject as any).recieveSignalData(clientID, mockSignalData);
+            (subject as any).receiveSignalData(clientID, mockSignalData);
 
             // Verify signal data was delivered to peer
             td.verify(mockSignal(mockSignalData));
         });
 
+        it("Should create a new peer if one doesn't already exist", function() {
+            // Stub out HostPeerManager#createPeer
+            const mockSetupPeer = td.function();
+            (subject as any).setupPeer = mockSetupPeer;
+
+            // Call HostPeerManager#receiveSignalData
+            (subject as any).receiveSignalData(clientID, mockSignalData);
+
+            // Verify that createPeer was called
+            td.verify(mockSetupPeer(clientID, mockSignalData));
+        });
+
+        it("Should be capable of receiving `n` signalData", function() {
+            // Create the mocked peer
+            const mockSignal = td.function();
+            const mockPeer = {
+                signal: mockSignal,
+                clientID,
+            };
+            (subject as any).peers.push(mockPeer);
+
+            const mockSignalArray = [];
+            for (let i = 0; i < 10; i++) {
+                mockSignalArray.push(Object.assign({}, mockSignalData, {
+                    id: i,
+                }));
+            }
+
+            // Push mock signal data to host peer manager
+            (subject as any).receiveSignalData(clientID, ...mockSignalArray);
+
+            // Verify signal data was delivered to peer
+            mockSignalArray.map((value) => td.verify(mockSignal(td.matchers.contains(value))));
+        });
+
+        /*********************** Helper Methods **************************/
+
+        // Add some peers with different clientID's
+        function createPeer(clientID: string) {
+            return Object.freeze({
+                clientID,
+            });
+        }
+
     });
 
-    describe("#createPeer", function() {
-        Assert.fail("Not implemented");
+    describe("#setupPeer", function() {
+        let mockSignal;
+
+        beforeEach(function() {
+            mockSignal = td.function();
+
+            // Mock out HostPeerManager#createPeer
+            (subject as any).createPeer = td.when(td.function()()).thenReturn({
+                on: td.function(),
+                signal: mockSignal,
+            });
+        });
+
+        it("Should add a new peer to array when called w/o signal data", function() {
+            // Call setupPeer
+            (subject as any).setupPeer(clientID);
+            const peerList: IEnhancedPeer[] = (subject as any).peers;
+            Assert.equal(peerList.length, 1, "Should have been a single peer added");
+            Assert.equal(peerList[0].clientID, clientID, "Peer clientID should have `clientID`");
+        });
+
+        it("Should add a new peer to array and signal it when calling with signalData", function() {
+            // Call setupPeer
+            (subject as any).setupPeer(clientID, mockSignalData);
+
+            // Verify peer creation
+            const peerList: IEnhancedPeer[] = (subject as any).peers;
+            Assert.equal(peerList.length, 1, "Should have been a single peer added");
+            Assert.equal(peerList[0].clientID, clientID, "Peer clientID should have `clientID`");
+
+            // Verify peer signaled
+            td.verify(mockSignal(mockSignalData));
+        });
+
+        it("Should be capable of setting up peer with multiple instances of signal data", function() {
+            const mockSignalArray = [];
+            for (let i = 0; i < 10; i++) {
+                mockSignalArray.push(Object.assign({}, mockSignalData, {
+                    id: i,
+                }));
+            }
+
+            // Call setupPeer
+            (subject as any).setupPeer(clientID, ...mockSignalArray);
+
+            // Verify peer creation
+            const peerList: IEnhancedPeer[] = (subject as any).peers;
+            Assert.equal(peerList.length, 1, "Should have been a single peer added");
+            Assert.equal(peerList[0].clientID, clientID, "Peer clientID should have `clientID`");
+
+            // Verify peer signaled
+            mockSignalArray.map((value) => td.verify(mockSignal(td.matchers.contains(value))));
+        });
+
     });
 
     describe("#watchPeer", function() {
-        Assert.fail("Not implemented");
+        // Assert.fail("Not implemented");
     });
 
     describe("#removePeer", function() {
-        Assert.fail("Not implemented");
+        // Assert.fail("Not implemented");
     });
 
 });
